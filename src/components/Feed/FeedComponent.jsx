b54f538615ea7762279bb596fd3c8c27891d185d
joshuaodoi <joshuaodoi@hotmail.co.uk>
2021-03-02 14:08:50 +0000

    feed

--- src/components/Feed/FeedComponent.jsx
+++ src/components/Feed/FeedComponent.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { CharacterCard } from '../STRETCH_Cards/CharacterCard';
-import './FeedComponent.css';
+import React from "react";
+import { CharacterCard } from "../STRETCH_Cards/CharacterCard";
+import "./FeedComponent.css";
 
 // TODO - make sure FeedComponent is expecting the right props!
-export const FeedComponent = ({ApiInfo}) => {
-
-    return <div className={'content'}>
-        {/* TODO - add a placeholder for an empty feed */}
-        {/* TODO - build up a list of results */}
-        {/* TODO [STRETCH] - update this list to be a list/grid of STRETCH_Cards */}
-        I'm the feed component
-    <CharacterCard ApiInfo = {ApiInfo}/>    
+export const FeedComponent = ({ ApiInfo }) => {
+  return (
+    <div className={"content"}>
+      {/* TODO - add a placeholder for an empty feed */}
+      {/* TODO - build up a list of results */}
+      {/* TODO [STRETCH] - update this list to be a list/grid of STRETCH_Cards */}
+      I'm the feed component
+      <div className="column2">
+        <div className="leftColumn">
+          <CharacterCard ApiInfo={ApiInfo} />
+        </div>
+        <div className="rightColumn">LIST OF OUTPUT</div>
+      </div>
     </div>
+  );
 };
