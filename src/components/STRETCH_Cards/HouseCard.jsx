b59aa4e13721a4e015c97e1f633c160680681818
joshuaodoi <joshuaodoi@hotmail.co.uk>
2021-03-03 13:19:23 +0000

    Api calls in house card

--- src/components/STRETCH_Cards/HouseCard.jsx
+++ src/components/STRETCH_Cards/HouseCard.jsx
@@ -25,6 +25,7 @@ const useStyles = makeStyles((theme) => ({
 export const HouseCard = (props) => {
   const [currentHouse, setCurrentHouse] = useState(null);
   const [characterName, setCharacterName] = useState([]);
+  const [houseOverlordName, setHouseOverlordName] = useState([]);
   const classes = useStyles();
 
   const changeHouse = (item) => {
@@ -35,31 +36,52 @@ export const HouseCard = (props) => {
     try {
       const response = await fetch(url);
       const data = await response.json();
+      setCharacterName(data.name);
     } catch (error) {
-      return console.log(error.message);
+      setCharacterName("");
+      console.log(`Error: ${error.message}`);
+    }
+  };
+
+  const getHouseOverlordName = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.name);
+      if (data.name === "") {
+        setHouseOverlordName("");
+      } else {
+        setHouseOverlordName(data.name);
+      }
+    } catch (error) {
+      setHouseOverlordName("");
+      console.log(`Error: ${error.message}`);
     }
   };
 
   return (
     <div className="column2">
       <div className="leftColumn">
-        {currentHouse && currentHouse.name}
-        {currentHouse && currentHouse.region}
-        {currentHouse && currentHouse.coatOfArms}
-        {/* {getCharacterName(currentHouse.currentLord)} */}
-
-        {/* {currentHouse && currentHouse.name}
-        {currentHouse && currentHouse.name}
-        {currentHouse && currentHouse.name}
-        {currentHouse && currentHouse.name}
-        {currentHouse && currentHouse.name} */}
+        Name: {currentHouse && currentHouse.name}
+        <br />
+        Region: {currentHouse && currentHouse.region}
+        <br />
+        Coat Of Arms: {currentHouse && currentHouse.coatOfArms}
+        <br />
+        CurrentLordName: {currentHouse && characterName}
+        <br />
+        House OverLordName: {currentHouse && houseOverlordName}
       </div>
       <div className="rightColumn">
         <Grid container spacing={3}>
           {props.ApiInfo.map((item) => (
             <Grid item xs={6}>
               <Paper
-                onClick={() => changeHouse(item)}
+                onClick={() => {
+                  changeHouse(item);
+                  getCharacterName(item.currentLord);
+                  getHouseOverlordName(item.overlord);
+                }}
                 className={`${classes.paper} box-shadow-img`}
               >
                 {item.name}
