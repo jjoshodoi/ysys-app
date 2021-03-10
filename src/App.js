8e7b7d610a9de9248c9b254b03d56d6911877525
joshuaodoi <joshuaodoi@hotmail.co.uk>
2021-03-10 12:34:02 +0000

    apipage

--- src/App.js
+++ src/App.js
@@ -8,20 +8,9 @@ import React, { useState, useEffect } from "react";
 import { Snackbar } from "@material-ui/core";
 
 function App() {
-  // TODO - this is the "main" component for our app, and it will include all the global state that we care about
-  //  This should include things like:
-  //  * the sidebar expanded state
-  //  * the selected category (books/characters/houses)
-  //  * the feed results
-
-  // TODO [STRETCH] - This could also include
-  //  * the search term (if there is one)
   //  * whether the app is fetching data (loading)
   //  * any additional filters (number of results/filter terms in query string)
 
-  //  Each part of the state will need to be passed down to its respective component(s) in order for it
-  //  to be displayed/updated in the correct part of the UI
-  //  * E.g. maybe you can expand/collapse the sidebar from the header and the feed, as well as the sidebar itself
   //  This means that the state will need to be accessed/updated from all of these components!
 
   // TODO [STRETCH] - implement loading state and pass to FeedComponent
